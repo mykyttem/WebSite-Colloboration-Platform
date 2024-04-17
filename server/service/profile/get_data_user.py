c5bedef0b90479675bfe1de17c60b3f1bee7749b
Asta111111 <bohdanbobkov202@gmail.com>
2024-04-17 11:36:46 +0300

    change information profile

--- server/service/profile/get_data_user.py
+++ server/service/profile/get_data_user.py
@@ -1,14 +1,13 @@
-from flask import Blueprint, jsonify, session
+from flask import Blueprint, jsonify, session, request
 from models.users import Users
 from database import db
 
 profile_bp = Blueprint("profile", __name__)
 
 
-@profile_bp.route("/", methods=["GET"])
+@profile_bp.route("", methods=["GET"])
 def get_data():
     id_user = session["user_id"]
-    print(id_user)
 
     user = db.session.query(Users).filter(Users.id == id_user).first()
     user_dict = {
@@ -21,5 +20,30 @@ def get_data():
 
     if user:
         return jsonify(user_dict), 200
+    else:
+        return jsonify(message="User not found"), 404
+    
+
+@profile_bp.route("", methods=["POST"])
+def update_data():
+    if id_user not in session:
+        return jsonify(message="User not authenticated"), 401
+
+    id_user = session["user_id"]
+    data = request.json
+
+    user = db.session.query(Users).filter(Users.id == id_user).first()
+
+    if user is not None:
+        if "username" in data:
+            user.username = data["username"]
+        if "email" in data:
+            user.email = data["email"]
+        if "password" in data:
+            user.password = data["password"]
+
+        db.session.commit()
+
+        return jsonify(message="User data updated successfully"), 200
     else:
         return jsonify(message="User not found"), 404
