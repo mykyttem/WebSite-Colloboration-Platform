2c64a7b4b2b546d371ae430bcfa69f76a55d2d14
Asta111111 <bohdanbobkov202@gmail.com>
2024-04-25 09:14:50 +0300

    updated server profile: added getting projects which created by user

--- server/service/profile/create_project.py
+++ server/service/profile/create_project.py
@@ -1,4 +1,4 @@
-from flask import jsonify, request
+from flask import jsonify, request, session
 from models.projects import Projects
 from database import db
 
@@ -6,14 +6,33 @@ from database import db
 def save_project():
     data = request.json
 
+    id_user = session["user_id"]
     title = data.get("title")
     description = data.get("description")
     number_of_members = data.get("members")
     active = data.get("isActive")
     categories = data.get("categories")
     
-    new_project = Projects(title=title, description=description, number_of_members=number_of_members, active=active, categories=categories)
+    new_project = Projects(title=title, description=description, number_of_members=number_of_members, active=active, categories=categories, user_id=id_user)
     db.session.add(new_project)
     db.session.commit()
+    
 
     return jsonify(message="Project created successful"), 200
+
+
+def get_projects_users():
+    id_user = session["user_id"]
+    
+    projects = db.session.query(Projects).filter(Projects.user_id == id_user).first()
+    projects_dict = {
+        "title": projects.title,
+        "description": projects.description,
+        "number_of_members": projects.number_of_members,
+        "active": projects.active,
+        "categories": projects.categories,
+        "date": projects.date
+    }
+
+
+    return jsonify(projects_dict), 200
