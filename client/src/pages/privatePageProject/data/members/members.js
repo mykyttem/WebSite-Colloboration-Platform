4ba01e9544de41c584995085cf709f0d9ac0abf5
chamois1 <nrabicenko5@gmail.com>
2024-05-12 13:40:50 +0300

    Updated private page project: added added a separate tab for members

---
+++ client/src/pages/privatePageProject/data/members/members.js
@@ -0,0 +1,12 @@
+const Members = ({members, numberMembers}) => {
+    return (
+        <>
+            <h2>Members</h2>
+            <p>Members: {members}</p>
+            <p>Number of Members: {numberMembers}</p>
+        </>
+    )
+}
+
+
+export default Members;
