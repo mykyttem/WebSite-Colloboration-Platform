580248a9e543c5d9c83e6afb3e824c5e86f0a49f
chamois1 <nrabicenko5@gmail.com>
2024-05-12 14:40:51 +0300

    Updated nav bar: added checking if user auth, display profile link, else buttons for auth

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { CheckAuth } from '../requests/fetchAuth';
 import "./navBar.css";
 
 
 const NavBar = () => {
+  const [authChecked, setAuthChecked] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const responseCheckAuth = await CheckAuth();
+        if (responseCheckAuth.status === 200) {
+          setLoggedIn(true);
+        } else {
+          setLoggedIn(false);
+        }
+        setAuthChecked(true);
+      } catch (error) {
+        console.error("Error checking authentication:", error);
+        setLoggedIn(false);
+        setAuthChecked(true);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (!authChecked) {
+    return null;
+  }
+
+ 
   return (
     <header className="header_main">
       <div>
@@ -17,12 +45,20 @@ const NavBar = () => {
         <a href="/notifications" style={{ color: 'transparent' }}>
           <button className="mail"><samp>&#9993;</samp></button>
         </a>
-        <a href="/sign-up" style={{ color: 'transparent' }}>
-          <button className="sign-up">Sign Up</button>
-        </a>
-        <a href="/sign-in" style={{ color: 'transparent' }}>
-          <button className="sign-in">Sign In</button>
-        </a>
+        {loggedIn ? (
+          <div>
+            <a href="/profile">Profile</a>
+          </div>
+        ) : (
+          <div>
+            <a href="/sign-up" style={{ color: 'transparent' }}>
+              <button className="sign-up">Sign Up</button>
+            </a>
+            <a href="/sign-in" style={{ color: 'transparent' }}>
+              <button className="sign-in">Sign In</button>
+            </a>
+          </div>
+        )}
       </div>
     </header>
   );
