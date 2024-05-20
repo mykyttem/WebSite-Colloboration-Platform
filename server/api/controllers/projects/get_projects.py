0658ab304da1740305b9a9e9d74f39074b7aadeb
asta111111 <bohdanbobkov202@gmail.com>
2024-05-20 12:39:10 +0300

    display only title, desc date

--- server/api/controllers/projects/get_projects.py
+++ server/api/controllers/projects/get_projects.py
@@ -1,14 +1,30 @@
 from flask import jsonify
 from ...models.projects import Projects
 from ...database.database_base import db
-from ...utils.project import serialize_project
+from ...utils.logging import logger
 
 
 def get_data_project():
-    projects = db.session.query(Projects).all()
+    try:
+        # Select only the required columns
+        columns = [Projects.title, Projects.description, Projects.date]
+        projects = db.session.query(*columns).all()
 
-    if projects:
-        serialized_projects = [serialize_project(project) for project in projects]
-        return jsonify(serialized_projects), 200
-    else:
-        return jsonify(message="Projects not found"), 404
+        if projects:
+            # Serialize the data
+            serialized_projects = [
+                {
+                    "Title": project.title,
+                    "Description": project.description,
+                    "Date": project.date.strftime("%Y-%m-%d %H:%M:%S") if project.date else None
+                }
+                for project in projects
+            ]
+
+            return jsonify(serialized_projects), 200
+        else:
+            logger.warn("Projects not found")
+            return jsonify(message="Projects not found"), 404
+    except Exception as e:
+        logger.error(f"Error retrieving projects: {e}")
+        return jsonify(message="Internal Server Error"), 500
